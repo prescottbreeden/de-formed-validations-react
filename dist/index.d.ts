e812445826a8b6673c69130c5e7e45e0ded26ced
bobross <androgenagonist@gmail.com>
2021-09-13 01:45:54 -0700

    updated tarball

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,19 +1,4 @@
-/// <reference types="react" />
-import { GetAllErrors, GetError, GetFieldValid, ResetValidationState, Validate, ValidateAll, ValidateAllIfTrue, ValidateIfTrue, ValidateOnBlur, ValidateOnChange, ValidationSchema } from './types';
+import { BaseValidation } from '@de-formed/base';
+import { ValidationSchema } from './types';
 export * from './types';
-export declare const useValidation: <S>(validationSchema: ValidationSchema<S>) => {
-    getAllErrors: GetAllErrors<S>;
-    getError: GetError<S>;
-    getFieldValid: GetFieldValid<S>;
-    isValid: boolean;
-    resetValidationState: ResetValidationState;
-    setValidationState: import("react").Dispatch<import("react").SetStateAction<import("@de-formed/base").ValidationState>>;
-    validate: Validate<S>;
-    validateAll: ValidateAll<S>;
-    validateAllIfTrue: ValidateAllIfTrue<S>;
-    validateIfTrue: ValidateIfTrue<S>;
-    validateOnBlur: ValidateOnBlur<S>;
-    validateOnChange: ValidateOnChange<S>;
-    validationErrors: string[];
-    validationState: import("@de-formed/base").ValidationState;
-};
+export declare const useValidation: <S>(validationSchema: ValidationSchema<S>) => BaseValidation<S>;
